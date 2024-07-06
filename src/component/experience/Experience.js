30b526f3dbab817c9231b9f16822bf9dc31c0d91
Folarinjr <folarinjunior2@gmail.com>
2024-07-06 16:37:02 +0100

    Update experience

--- src/component/experience/Experience.js
+++ src/component/experience/Experience.js
@@ -1,113 +1,127 @@
-import React from 'react'
-import './Experience.css'
-import {BsPatchCheckFill} from 'react-icons/bs'
+import React from "react";
+import "./Experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
-import { useInView } from 'react-intersection-observer'
+import { useInView } from "react-intersection-observer";
 
 const Experience = () => {
-  const {ref, inView} = useInView();
+  const { ref, inView } = useInView();
   return (
-    <section 
-      id='experience'
-      ref={ref}
-    >
+    <section id="experience" ref={ref}>
       <h5>My Skills</h5>
       <h2>My Experience</h2>
 
-      <div className={inView ? "container experience__container" : "container none"}>
+      <div
+        className={
+          inView ? "container experience__container" : "container none"
+        }
+      >
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
 
           <div className="experience__content">
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>TailWindCSS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>SASS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>JAVASCRIPT</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>TYPESCRIPT</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>REACT JS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>REACT NATIVE</h4>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>NEXT JS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>REACT NATIVE</h4>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>KOTLIN(JETPACK COMPOSE)</h4>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>SwiftUI</h4>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>GIT</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>STYLED COMPONENTS</h4>
               </div>
             </article>
 
             <article className="experience__details">
-              <BsPatchCheckFill className='experience__details-icon' />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>REDUX</h4>
               </div>
             </article>
           </div>
-
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
